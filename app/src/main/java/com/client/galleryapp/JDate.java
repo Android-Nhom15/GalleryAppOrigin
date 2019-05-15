c0de5fae524345d422ee598afa49b104a6512ac3
TranBaNgoc <1612431@student.hcmus.edu.vn>
2019-05-15 22:18:32 +0700

    Hoan thanh hien thi anh theo thang

---
+++ app/src/main/java/com/client/galleryapp/JDate.java
@@ -0,0 +1,35 @@
+package com.client.galleryapp;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class JDate {
+    private Date date;
+    private ArrayList<File> fileImage;
+
+    public JDate(Date date, ArrayList<File> fileImage) {
+        this.date = date;
+        this.fileImage = fileImage;
+    }
+
+    public JDate() {
+        fileImage = new ArrayList<>();
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public ArrayList<File> getFileImage() {
+        return fileImage;
+    }
+
+    public void setFileImage(ArrayList<File> fileImage) {
+        this.fileImage = fileImage;
+    }
+}
---
+++ app/src/main/java/com/client/galleryapp/OneMonthImageAdapter.java
@@ -0,0 +1,73 @@
+package com.client.galleryapp;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class OneMonthImageAdapter extends BaseAdapter {
+
+    private Context context;
+    private int layout;
+    private ArrayList<File> fileImages;
+
+    public OneMonthImageAdapter(Context context, ArrayList<File> fileImages) {
+        this.context = context;
+        this.fileImages = fileImages;
+    }
+
+    @Override
+    public int getCount() {
+        return fileImages.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    //Đâu
+    //Cứ như merge thôi. Lấy cái
+    // Sao pull fail
+
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        convertView = inflater.inflate(R.layout.item_2_showbydate, null);
+        ImageView iv_photo = (ImageView)convertView.findViewById(R.id.imageViewByDate);
+
+        Glide.with(context).load(Uri.fromFile(fileImages.get(position)))
+                .centerCrop()
+                .placeholder(R.drawable.waitting_for_load)
+                .into(iv_photo);
+
+        iv_photo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, FullScreenPhoto.class);
+                intent.putExtra("SENDER_KEY", "MyFragment");
+                intent.putExtra("img", position);
+                intent.putExtra("list", fileImages);
+                context.startActivity(intent)   ;
+            }
+        });
+
+        return convertView;
+    }
+
+}
