2a9f8f4e4a0ae452d34d93545bbe43d48c0f7b66
TranBaNgoc <1612431@student.hcmus.edu.vn>
2019-06-03 02:07:44 +0700

    Update blur activity

--- app/src/main/java/com/client/galleryapp/RenderScriptGaussianBlur.java
+++ app/src/main/java/com/client/galleryapp/RenderScriptGaussianBlur.java
@@ -17,20 +17,60 @@ import android.support.annotation.NonNull;
 public class RenderScriptGaussianBlur {
 
     private RenderScript renderScript;
+    private static final float BITMAP_SCALE = 0.4f;
+
+    //Set the radius of the Blur. Supported range 0 < radius <= 25
+    private static float BLUR_RADIUS = 10.5f;
 
     public RenderScriptGaussianBlur(@NonNull Context context) {
         this.renderScript = RenderScript.create(context);
     }
 
     public Bitmap gaussianBlur(@IntRange(from = 1, to = 25) int radius, Bitmap original) {
-        Allocation input = Allocation.createFromBitmap(renderScript, original);
-        Allocation output = Allocation.createTyped(renderScript, input.getType());
-        ScriptIntrinsicBlur scriptIntrinsicBlur = ScriptIntrinsicBlur.create(renderScript, Element.U8_4(renderScript));
-        scriptIntrinsicBlur.setRadius(radius);
-        scriptIntrinsicBlur.setInput(input);
-        scriptIntrinsicBlur.forEach(output);
-        output.copyTo(original);
-        return original;
+//        Allocation input = Allocation.createFromBitmap(renderScript, original);
+//        Allocation output = Allocation.createTyped(renderScript, input.getType());
+//        ScriptIntrinsicBlur scriptIntrinsicBlur = ScriptIntrinsicBlur.create(renderScript, Element.U8_4(renderScript));
+//        scriptIntrinsicBlur.setRadius(radius);
+//        scriptIntrinsicBlur.setInput(input);
+//        scriptIntrinsicBlur.forEach(output);
+//        output.copyTo(original);
+//        return original;
+
+        Bitmap outputBitmap = null;
+
+        if (original != null) {
+
+            if (radius == 0) {
+                return original;
+            }
+
+            if (radius < 1) {
+                radius = 1;
+            }
+
+            if (radius > 25) {
+                radius = 25;
+            }
+
+            BLUR_RADIUS = radius;
+
+            int width = Math.round(original.getWidth() * BITMAP_SCALE);
+            int height = Math.round(original.getHeight() * BITMAP_SCALE);
+
+            Bitmap inputBitmap = Bitmap.createScaledBitmap(original, width, height, false);
+            outputBitmap = Bitmap.createBitmap(inputBitmap);
+
+
+            ScriptIntrinsicBlur theIntrinsic = ScriptIntrinsicBlur.create(renderScript, Element.U8_4(renderScript));
+            Allocation tmpIn = Allocation.createFromBitmap(renderScript, inputBitmap);
+            Allocation tmpOut = Allocation.createFromBitmap(renderScript, outputBitmap);
+            theIntrinsic.setRadius(BLUR_RADIUS);
+            theIntrinsic.setInput(tmpIn);
+            theIntrinsic.forEach(tmpOut);
+            tmpOut.copyTo(outputBitmap);
+        }
+
+        return outputBitmap;
     }
 
 }
